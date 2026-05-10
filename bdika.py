0dc81892548eed252cd4ef7d8f5b54c41f12cbff
somatb1994 <somatb1994@gmail.com>
2026-05-10 12:52:45 +0300

    eifo ze

--- bdika.py
+++ bdika.py
@@ -1,7 +1,8 @@
-a = 5
-b = 3
-c = 2 
-d = 8
-e = 3
+a = float(input("enter a number: "))
+print("ur number is: ",(a))
 
-print((a>d)| (d>c)| (a>b) & (b==e) & (c!=d))
+b = str(input("enter a number: "))
+print("ur number is: ",b ,"and the type is: " ,type(b))
+
+c = input("enter name: ")
+print("ur name is", c,"and the lengh is :", len(c))
